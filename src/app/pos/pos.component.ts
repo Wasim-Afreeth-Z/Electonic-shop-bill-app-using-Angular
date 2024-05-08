f6402aada32f7c5eaedcd4ef807a2489a36d156d
Wasim-Afreeth-Z <wasimafreeth36@gmail.com>
2024-05-08 15:10:59 +0530

    pos add to cart find change

--- src/app/pos/pos.component.ts
+++ src/app/pos/pos.component.ts
@@ -83,7 +83,7 @@ export class POSComponent {
         const response = this.commonService.decryptData({ data: res })
         // console.log(response);
         this.productpos = response.result
-        // console.log(this.productpos);
+        console.log(this.productpos);
 
       }
     }
@@ -154,7 +154,10 @@ export class POSComponent {
 
   // !add to cart
   addtoCart(product: any): void {
-    const index = this.cart.findIndex(item => item === product);
+    const index = this.cart.findIndex(item => item.sellingPrice === product.sellingPrice);
+    console.log(index);
+    
+    
     if (index !== -1) {
       if (this.cart[index].q < 10) {
         this.cart[index].q++;
